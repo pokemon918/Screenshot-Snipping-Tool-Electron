1cdce36ae3a9615a87dde717d5f4dc86e06b28b4
marsinlegend <j27394645@gmail.com>
2020-05-06 17:05:49 +0300

    Using the desktopCapturer API

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require('electron');
+const { ipcRenderer, remote } = require('electron');
 
 let win;
 
--- src/App.js
+++ src/App.js
@@ -4,6 +4,27 @@ import { Navbar, Button, Alignment, Icon } from '@blueprintjs/core';
 import './App.css';
 
 function App() {
+  const onSnipClick = async () => {
+      console.log('todo: making screenshot');
+      console.log(window);
+      const { desktopCapturer, remote } = window.require('electron');
+      const screen = remote.screen;
+
+      try {
+          const sources = await desktopCapturer.getSources({
+              types: ['screen']
+          });
+          const entireScreenSource = sources
+              .find(source => source.name === 'Entire Screen' || source.name === 'Screen 1');
+
+          if (entireScreenSource) {
+              console.log(entireScreenSource);
+          }
+      } catch (err) {
+          console.error(err);
+      }
+  };
+
   return (
     <div className="App">
       <Navbar className="bp3-dark">
@@ -12,6 +33,12 @@ function App() {
           <Navbar.Divider />
           <Button className="bp3-minimal" icon="settings" text="Settings" />
           <Button className="bp3-minimal" icon="help" text="About" />
+          <Button
+              className="bp3-minimal"
+              icon="camera"
+              text="Snip"
+              onClick={onSnipClick}
+          />
         </Navbar.Group>
       </Navbar>
       <main className="App-main">
